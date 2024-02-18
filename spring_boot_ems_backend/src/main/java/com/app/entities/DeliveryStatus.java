a52fa8e9d7282489428f17f6f74c8b2049598c95
KD1-80095 <aniketjadhav241299@gmail.com>
2024-02-19 02:37:17 +0530

    Uploaded all entities

---
+++ spring_boot_ems_backend/src/main/java/com/app/entities/DeliveryStatus.java
@@ -0,0 +1,5 @@
+package com.app.entities;
+
+public enum DeliveryStatus {
+	PLACED, CONFIRMED, COOKING, OUTFORDELIVERY, DELIVERED
+}
---
+++ spring_boot_ems_backend/src/main/java/com/app/entities/LoginCred.java
@@ -0,0 +1,18 @@
+package com.app.entities;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginCred {
+	
+	private String email;
+	private String password;
+
+}
